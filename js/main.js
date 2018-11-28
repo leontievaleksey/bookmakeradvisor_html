026436f0ddfb0850553be76daf854fa2a878a790
Aleksey <web-a.leon@ukr.net>
2018-11-28 06:07:02 +0200

    Home page: added hover effects, search, lang dropdown. Promotions: started

--- js/main.js
+++ js/main.js
@@ -2,15 +2,32 @@
 
 	$('#header_dropdown_button').on('click', function(evt) {
 
-		if ( $(this).hasClass('opened') ) {
-
-		}
-
 		$(this).toggleClass('opened');
 		$("#header_main_menu").slideToggle();
 
+	});
 
+	$('#header_search_button').on('click', function(evt) {
 
+		$('#header_search_menu, #search_overlay').slideDown(function(){
+			$('#header_quick_links_menu').slideDown();
+		});
+
+	});
+
+	$('#header_lang_button').on('click', function(evt) {
+
+		$(this).toggleClass('active');
+		$("#header_lang_menu").slideToggle();
+
+	});
+
+	$('#header_search_close, #search_overlay').on('click', function(evt) {
+
+		$('#header_quick_links_menu').slideUp(function(){
+			$('#header_search_menu, #search_overlay').slideUp();
+		});
+		
 	});
 
 })(jQuery);
