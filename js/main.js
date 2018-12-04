5069e8642baa76c0a83b955f2ded9ce21c215f74
Aleksey <web-a.leon@ukr.net>
2018-12-04 06:05:31 +0200

    pages: best_bookmakers, paypal, comeon(in process)

--- js/main.js
+++ js/main.js
@@ -30,4 +30,15 @@
 		
 	});
 
+	$('#banner_menu_carousel').slick({
+	  dots: false,
+	  infinite: false,
+	  speed: 300,
+	  slidesToShow: 5,
+	  centerMode: false,
+	  variableWidth: true,
+	  prevArrow: '<i class="fa fa-angle-left banner_carousel_arrow_left banner_carousel_arrow"></i>',
+	  nextArrow: '<i class="fa fa-angle-right banner_carousel_arrow_right banner_carousel_arrow"></i>'
+	});
+
 })(jQuery);
